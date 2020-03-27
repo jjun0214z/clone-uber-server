eb149b47857d063ee226c8261432e05711d2fbc1
park jun min <jjun0214z@nomadconnection.com>
2020-03-27 14:16:50 +0900

    graphql arguments 타입체크

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,10 +1,10 @@
-import { Greeting } from "src/types/graph";
+import { SayHelloResponse, SayHelloQueryArgs } from "src/types/graph";
 
 const resolvers = {
   Query: {
-    sayHello: (): Greeting => {
+    sayHello: (_: any, args: SayHelloQueryArgs): SayHelloResponse => {
       return {
-        text: "Hey hello how are you",
+        text: `Hello ${args.name}`,
         error: false
       };
     }
