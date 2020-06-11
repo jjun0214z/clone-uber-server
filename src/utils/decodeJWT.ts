891b7a331bfa4ba843828be5c3c7183409d6939c
min0214 <jjun0214z@nomadconnection.com>
2020-06-11 16:21:11 +0900

    custom auth middeleware on express

--- src/utils/decodeJWT.ts
+++ src/utils/decodeJWT.ts
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
-import User from "src/entities/User";
+import User from "../entities/User";
 
 const decodeJWT = async (token: string): Promise<User | undefined> => {
   try {
-    const decode = jwt.verify(token, process.env.JWT_TOKEN || "");
-    const { id } = decode;
+    const decoded: any = jwt.verify(token, process.env.JWT_TOKEN || "");
+    const { id } = decoded;
     const user = await User.findOne({ id });
     return user;
   } catch (error) {
