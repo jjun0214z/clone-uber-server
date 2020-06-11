8e9880d73f9a47636ee60e95623ace4b00c47a61
min0214 <jjun0214z@nomadconnection.com>
2020-06-11 10:44:19 +0900

    EmailSignUp resolve

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,39 @@
+import { Resolvers } from "../../../types/resolvers";
+import { EmailSignUpMutationArgs, EmailSignUpResponse } from "src/types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignUp: async (
+      _,
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      const { email } = args;
+      try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return {
+            ok: true,
+            error: "You should log in instead",
+            token: null,
+          };
+        } else {
+          // const newUser = await User.create({ ...args }).save();
+          return {
+            ok: true,
+            error: null,
+            token: "Comming soon",
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.messeage,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
