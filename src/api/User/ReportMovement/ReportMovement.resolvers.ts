3e52a73b1e4548cf5cb54fdfdcff481fa7d25f6e
min0214 <jjun0214z@nomadconnection.com>
2020-06-17 11:11:00 +0900

    ReportMovement resolver

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,27 +1,23 @@
+import User from "../../../entities/User";
+import {
+  ReportMovementResponse,
+  ReportMovementMutationArgs,
+} from "src/types/graph";
 import { Resolvers } from "src/types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
-import {
-  UpdateMyProfileMutationArgs,
-  UpdateMyProfileResponse,
-} from "src/types/graph";
-import User from "../../../entities/User";
 import cleanNullArgs from "../../../utils/cleanNullArgs";
 
 const resolvers: Resolvers = {
   Mutation: {
-    UpdateMyProfile: privateResolver(
+    ReportMovement: privateResolver(
       async (
         _,
-        args: UpdateMyProfileMutationArgs,
+        args: ReportMovementMutationArgs,
         { req }
-      ): Promise<UpdateMyProfileResponse> => {
+      ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
         try {
-          if (args.password !== null) {
-            user.password = args.password;
-            user.save();
-          }
           await User.update({ id: user.id }, { ...notNull });
           return {
             ok: true,
